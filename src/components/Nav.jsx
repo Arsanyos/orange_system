dad6f58873c1c501752a15e955c08b34cf30f54a
Arsanyos <arsanyos1600@gmail.com>
2022-09-05 15:32:53 +0300

    add firebase

---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Nav(){
+    return(
+        <div className='nav-container'>
+            <h1>Navigateion</h1>
+        </div>
+    )
+}
+
+export default Nav;
